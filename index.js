943cb43d838ec2576eec75eee05b767e9ca12894
Viktoria <doduchenko29@gmail.com>
2021-05-07 14:49:55 +0300

    add

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 let sum = 0;
+
 for (let i = 0; i <= 1000; i++) {
   sum += i;
 }
 
 let x = sum / 1234;
-let y = x % 2 === 0;
-let b = Math.trunc(sum / 1234);
+let b = Math.trunc(x);
+let y = x % 2;
 
-/*let c = b > y;
-console.log(c);*/
-
-if (b > y) {
-  console.log(true);
-} else {
-  console.log(false);
-}
+let c = b > y;
+console.log(c);
